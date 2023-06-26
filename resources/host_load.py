82289a0b83377e193f2a02fa25264b0212183ad9
baniasadi-m <masoud.baniasadi@gmail.com>
2023-06-26 16:32:42 +0330

    updating load resource of agent

--- resources/host_load.py
+++ resources/host_load.py
@@ -1,15 +1,13 @@
 from flask import jsonify, make_response, request
 from  flask_restful import Resource
+from ..config import Config
 import shutil
 import os
 import psutil
 
 class GetLoad(Resource):
     def get(self):
-        # source_addr = get_remote_addr(request)
-        # source_addr = '127.0.0.1'
-        # if source_addr in WHITE_LIST_IP:
-        if True:
+        if request.remote_addr in Config.WHITE_LIST_ACCESS_IP:
             total, used, free = shutil.disk_usage("/")
             free_root_mb = free // (2**20)
             total, used, free = shutil.disk_usage("/home")
@@ -19,7 +17,7 @@ class GetLoad(Resource):
             cpu_count = os.cpu_count() 
             
             if int(free_root_mb) > 2000 and int(free_home_mb) > 5000:
-                return make_response(jsonify({'load': float(((float(load_avg[2])/float(cpu_count))*200) + (float(free_memory_percent) * 1.5))}),200)
+                return make_response(jsonify({'status':'1','load': float(((float(load_avg[2])/float(cpu_count))*200) + (float(free_memory_percent) * 1.5))}),200)
                 
-            return make_response(jsonify({'load': 10000,'root': free_root_mb,'home':free_home_mb}),200)
+            return make_response(jsonify({'status':'0','load': 10000,'root': free_root_mb,'home':free_home_mb}),200)
         return make_response(jsonify({"message":"not Found(403)"}),403)
