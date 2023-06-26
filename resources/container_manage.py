aa5ab9faaab103263fc3f45008252baf9c798321
baniasadi-m <masoud.baniasadi@gmail.com>
2023-06-26 11:16:45 +0330

    get container status and remove container completed

--- resources/container_manage.py
+++ resources/container_manage.py
@@ -20,15 +20,32 @@ post_parser.add_argument('volumes', dest='volumes',type=dict,required=True,help=
 post_parser.add_argument('ports', dest='ports',type=dict,required=True,help='ports to expose')
 
 class ContainerManager(Resource):
-    def get(self):
+    def get(self,id=None):
         if request.remote_addr in Config.WHITE_LIST_ACCESS_IP:
             client = docker.from_env()
-            containers_list=[]
-            for container in client.containers.list():
-                containers_list.append(container.name)
-            containers={}
-            containers.update({'containers': containers_list})
-            return make_response(jsonify(containers),200)
+            if id == None:
+                client = docker.from_env()
+                containers_list=[]
+                for container in client.containers.list():
+                    containers_list.append(container.name)
+                containers={}
+                containers.update({'containers': containers_list})
+                return make_response(jsonify(containers),200)
+            result = {}
+            try:
+                container = client.containers.get(id)
+            except Exception as e:
+                print(e)
+                return make_response(jsonify({'result':'getting container status Failed'}),500)
+            result.update({'container_spec':{
+                'id': f"{container.id}",
+                'short_id': f"{container.short_id}",
+                'name': f"{container.name}",
+                'status': f"{container.status}"
+            }})
+            return make_response(jsonify(result),200)
+            
+
         return make_response(jsonify({'result':'Not Found(403)'}),403)
         
 
@@ -45,12 +62,40 @@ class ContainerManager(Resource):
                 container = client.containers.run(image=f"{args['container_image']}",detach=True,name=f"{args['container_name']}", volumes=volumes,ports=ports,
                         environment=environment,mem_limit=f"{args['memory']}",cpuset_cpus= args['cpuset'])
                 container.logs()
-                return make_response(jsonify({'result':'OK'}),200)
+                result ={}
+                print(container.status)
+                if container.status == "exited":
+                    return make_response(jsonify({"status":"0","result":"container creating error"}),500)
+
+                else:
+                    result.update({'container_spec':{
+                        'id': f"{container.id}",
+                        'short_id': f"{container.short_id}",
+                        'name': f"{container.name}",
+                        'status': f"{container.status}"
+                    }})
+                    result.update({"status":"1","result":"container created"})
+                    return make_response(jsonify(result),200)                    
             except Exception as e:
                 print('Run container Exception: ',e)
                 return make_response(jsonify({'result':'create container error'}),500)
 
         return make_response(jsonify({'result':'Not Found(403)'}),403)
+
+    def delete(self,id):
+        if request.remote_addr in Config.WHITE_LIST_ACCESS_IP:
+            client = docker.from_env()
+            try:
+                container = client.containers.get(id)
+                exit_code = container.remove(v=True, force=True)
+                print(exit_code)
+                return make_response(jsonify({'result':'OK','message':f"{exit_code}"}),200)
+            except Exception as e:
+                print("run command error: ",e)
+                return make_response(jsonify({'result':'running command error'}),500)
+
+        return make_response(jsonify({'result':'Not Found(403)'}),403)
+
     def put(self,id):
         if request.remote_addr in Config.WHITE_LIST_ACCESS_IP:
             args =  put_parser.parse_args()
@@ -67,18 +112,3 @@ class ContainerManager(Resource):
 
         return make_response(jsonify({'result':'Not Found(403)'}),403)
 
-    def delete(self,id):
-        if request.remote_addr in Config.WHITE_LIST_ACCESS_IP:
-            args =  put_parser.parse_args()
-            print(id,args)
-            client = docker.from_env()
-            try:
-                container = client.containers.get(id)
-                exit_code = container.remove(v=True, force=True)
-                print(exit_code)
-                return make_response(jsonify({'result':'OK'}),200)
-            except Exception as e:
-                print("run command error: ",e)
-                return make_response(jsonify({'result':'running command error'}),500)
-
-        return make_response(jsonify({'result':'Not Found(403)'}),403)
